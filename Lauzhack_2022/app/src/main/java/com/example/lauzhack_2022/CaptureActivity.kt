d372151dd4ce86c65486e1045505c461e61c2b98
Matthieu <matthieu.andre@epfl.ch>
2022-12-04 07:04:44 +0100

    progress

---
+++ Lauzhack_2022/app/src/main/java/com/example/lauzhack_2022/CaptureActivity.kt
@@ -0,0 +1,208 @@
+package com.example.lauzhack_2022
+
+
+import android.Manifest
+import android.R.string
+import android.content.ContentValues
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_FORWARD_RESULT
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.lauzhack_2022.databinding.ActivityCaptureBinding
+import com.example.lauzhack_2022.databinding.ActivityCaptureBinding.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+typealias LumaListener = (luma: Double) -> Unit
+
+
+class CaptureActivity : AppCompatActivity() {
+    private val TAG = "CAPTURE_TAG"
+    val URI_KEY = "URI_KEY"
+    private lateinit var viewBinding: ActivityCaptureBinding
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {
+
+//        val folder: File = Environment.getExternalStorageDirectory()
+//        val fileName = folder.path + "/Pictures/CameraX-Image/current_reciept.jpg"
+//
+//        val myFile = File(fileName)
+//        if (myFile.exists()) myFile.delete()
+
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+//        val name = FILENAME_FORMAT
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    Log.d(TAG, output.savedUri.toString())
+                    val intent =
+                        Intent(applicationContext, EditActivity::class.java)
+                    intent.flags = intent.flags or FLAG_ACTIVITY_FORWARD_RESULT
+                    intent.putExtra(URI_KEY, output.savedUri)
+                    startActivity(intent)
+//                    val msg = "Photo capture succeeded: ${output.savedUri}"
+//                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+//                    Log.d(TAG, msg)
+
+
+                }
+            }
+        )
+//        val myIntent = Intent(this, EditActivity::class.java)
+//        this.startActivity(myIntent)
+    }
+
+    private fun captureVideo() {}
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT =  "yyyy-MM-dd-HH-mm-ss-SSS"// "current_reciept" //"yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+}
+
---
+++ Lauzhack_2022/app/src/main/java/com/example/lauzhack_2022/EditActivity.kt
@@ -0,0 +1,97 @@
+package com.example.lauzhack_2022
+
+
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.lauzhack_2022.databinding.ActivityEditBinding
+import okhttp3.OkHttpClient
+import java.util.Base64.getEncoder
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.veryfi.android.VeryfiClientFactory
+import okhttp3.*
+import java.io.*
+import java.util.*
+
+class EditActivity : AppCompatActivity() {
+    val TAG = "EDIT_TAG"
+    val URI_KEY = "URI_KEY"
+    private val client = OkHttpClient()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val uri = intent.getParcelableExtra<Uri>(URI_KEY) //getParcelableExtra(URI_KEY)!!
+        Log.d(TAG, uri.toString())
+        binding.viewReceiptImage.setImageURI(uri)
+
+        val toCamera = findViewById<Button>(R.id.new_take)
+        toCamera.setOnClickListener { backToCapture() }
+
+        val toLearn = findViewById<Button>(R.id.ok)
+        toLearn.setOnClickListener{ backToMenu(uri) }
+
+
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun backToMenu(uri: Uri?){
+        val resolver = contentResolver
+        val stream = uri?.let { resolver.openInputStream(it) }
+        if (stream != null) {
+            run("http://128.179.151.225:5000", stream)
+        }
+//        val myIntent = Intent(this, MainActivity::class.java)
+//        this.startActivity(myIntent)
+    }
+
+    private fun backToCapture(){
+        val myIntent = Intent(this, CaptureActivity::class.java)
+        this.startActivity(myIntent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun run(url: String, stream: InputStream) {
+        var base64EncodedString: String? = ""
+        try {
+            base64EncodedString = Base64.getEncoder().encodeToString(stream.readBytes())
+//            Log.i("TAG", base64EncodedString)
+        } catch (e: IOException) {
+            e.message?.let { Log.e("TAG", it) }
+        }
+
+        val data = "{\"receipt\":\"".plus(base64EncodedString).plus("\"}")
+
+
+        val request = Request.Builder()
+            .url(url).post(RequestBody.create(MediaType.parse("application/json"), data))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.i("TAG", "hi")
+                throw e
+            }
+            override fun onResponse(call: Call, response: Response) {
+                response.body()?.string()?.let { Log.i(TAG, it) }
+            }
+        })
+    }
+
+}
